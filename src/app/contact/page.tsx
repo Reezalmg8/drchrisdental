1154a8c3ebefda5859b841b9d8984f695d50532f
Reezal <swansea73@gmail.com>
2024-11-01 10:56:00 +0800

    Updated contact page component

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,13 @@
+// src/app/aboutus/page.tsx
+import React from 'react';
+import {Contact} from '@/components/contact';
+
+const contactuspage: React.FC = () => {
+    return (
+        <div>
+            <Contact/>
+        </div>
+    );
+};
+
+export default contactuspage;
